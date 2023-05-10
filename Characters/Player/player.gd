d70e5ffdfcb4486741fc8b46805e0bea746fc674
digitbraul <digitbraul001@outlook.com>
2023-05-10 14:48:01 +0100

    Changed AnimationTree and added a TimeScale node

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 const MAX_SPEED = 40
-const ACCELERATION = 150
-const FRICTION = 500
-const RUN_SPEED_SCALE : float = 2.0
+const ACCELERATION = 100
+const FRICTION = 300
+const RUN_SPEED_SCALE : float = 1.4
 const START_POSTION : Vector2 = Vector2(0,1)
 
 @onready var animationPlayer = $AnimationPlayer
@@ -30,7 +30,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		# animation
 		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Run/BlendSpace2D/blend_position", input_vector)
 		# movement
 		velocity = velocity.move_toward(input_vector * MAX_SPEED * (1 + Input.get_action_strength("run")), ACCELERATION * delta)
 		# faster animation if shift preesed 
@@ -39,6 +39,8 @@ func _physics_process(delta):
 			speedScale = RUN_SPEED_SCALE
 		else:
 			speedScale = 1.0
+		# Set the animation speed scale according to the speedScale variable
+		animationTree.set("parameters/Run/runSpeedScale/scale", speedScale)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
